c064a661f29101afcd6681a910175d5165cdcc0d
joao vitor <joaovitorfguerra@gmail.com>
2026-03-20 22:43:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import backup3icsus as dados
+
+app = Flask(__name__)
+
+# Mapear procedimentos disponíveis
+procedimentos = {
+    "samu": dados.dfs1,
+    "samu_orientacao": dados.dfs2
+}
+
+# Mapear funções de gráficos
+graficos = {
+    "linhas": dados.plot_linhas,
+    "barras": dados.plot_barras,
+    "barras_stack": dados.plot_barras_stack,
+    "heatmap": dados.plot_heatmap,
+    "area": dados.plot_area
+}
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    grafico_html = None
+
+    if request.method == "POST":
+        proc = request.form["procedimento"]
+        tipo = request.form["grafico"]
+        dataset = request.form["dataset"]
+
+        dfs = procedimentos[proc]
+        func = graficos[tipo]
+
+        # CRIANDO FIGURA SEM SHOW()
+        fig = func(dfs, dataset, dados.cod_capitais)
+
+        try:
+            grafico_html = fig.to_html(full_html=False)
+        except:
+            grafico_html = "<p>Erro ao gerar gráfico</p>"
+
+    return render_template(
+        "index.html",
+        grafico=grafico_html,
+        procedimentos=procedimentos.keys(),
+        graficos=graficos.keys()
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
